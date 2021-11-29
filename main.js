6d84e816f1277750d0ff63b5a39d2762f8d575e6
matthiasdaues <md@matthiasdaues.de>
2021-11-29 18:50:14 +0100

    potential coverage implemented

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ import XYZ from 'ol/source/XYZ';
 import ImageWMS from 'ol/source/ImageWMS';
 import TileWMS from 'ol/source/TileWMS';
 import OSM from 'ol/source/OSM';
+import GeoTIFF from 'ol/source/GeoTIFF'
 
 // layer types
 import {Image as ImageLayer, Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
@@ -75,9 +76,7 @@ const locationStyleInactive = new Style({
     stroke: new Stroke({color: '#076859', width: 0.5}),
   })
 });
-
-// 01 2 2 gateway locations dynamic style functions
-// 01 2 2 1 gateway styled by status
+// 01 2 1 3 dynamic gateway location style by status
 const locationStyle = function (feature) {
   const styleTable = {
     "0": locationStyleInactive,
@@ -86,6 +85,12 @@ const locationStyle = function (feature) {
   };
   return styleTable[feature.get("status")] || locationStyleInactive
 };
+// 01 2 2 coverage styles
+// 01 2 2 1 potential coverage over all available locations
+const potentialCoverageStyle = new Style({
+  fill: new Fill({color: 'rgba( 42, 177, 184, 0.20 )'}),
+  stroke: new Stroke({color: 'rgba( 42, 177, 184, 1.00 )', width: 0.5})
+});
 
 // 02 define layers and content
 // 02 1 vector layers
@@ -99,7 +104,6 @@ const gatewayLocationsWFS = new VectorLayer({
     }),
   style: locationStyle
 });
-
 // 02 1 2 Gateway-Locations as WMS
 const gatewayLocationsWMS = new TileLayer({
   source: new TileWMS({
@@ -110,21 +114,38 @@ const gatewayLocationsWMS = new TileLayer({
   })
 });
 
-// 02 2 1 Potential Coverage from GeoJSON-File
-const potentialCoverage = new VectorLayer({
+// 02 1 3 Potential Coverage as WFS
+const potentialCoverageWFS = new VectorLayer({
   source: new VectorSource({
     format: new GeoJSON(),
-    url: '/home/matthiasdaues/Documents/@nextcloud/datenschoenheit/melita/Simulations/goetel/coverage_110dbm.json.txt'
+    url:'http://localhost:8080/geoserver/ows?service=wfs&' +
+        'version=1.1.0&request=GetFeature&typename=melita:potential_coverage_test&' +
+        'outputFormat=application/json&srsname=EPSG:3857&'
+    }),
+  style: potentialCoverageStyle
+});
+// 02 1 4 Potential Coverage as WMS
+const potentialCoverageWMS = new TileLayer({
+  source: new TileWMS({
+    url: 'http://localhost:8080/geoserver/melita/wms',
+    params: {'LAYERS': 'melita:potential_coverage_test'}, // for tiled WMS add ", 'TILED': true"
+    serverType: 'geoserver',
+    transition: 0,
   })
 });
+
+// 02 2 Raster layers
+// 02 2 1 active coverage
+
 
 // put all on a map
 new Map({
   target: 'map',
   layers: [
       cartoLightAll,
-      gatewayLocationsWFS,
-      potentialCoverage
+      //potentialCoverageWMS,
+      potentialCoverageWFS,
+      gatewayLocationsWFS
       //gatewayLocationsWMS
   ],
   view: new View({

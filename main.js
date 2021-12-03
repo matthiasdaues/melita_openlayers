4e41a6e8a9e1fe22ebb6d3bc2b05d73a61c85049
matthiasdaues <md@matthiasdaues.de>
2021-12-03 16:43:01 +0100

    December 3, 2021 4:43 PM

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ import GeoJSON from 'ol/format/GeoJSON';
 
 // controls
 import {defaults as defaultControls} from 'ol/control';
+import ScaleLine from 'ol/control/ScaleLine';
 import MousePosition from 'ol/control/MousePosition';
 
 // sources
@@ -203,5 +204,5 @@ new Map({
   }),
 });
 
-map.addControl(new ol.control.LayerSwitcher);
+map.addControl(new ScaleLine());
 

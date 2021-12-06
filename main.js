790698d8309e6ee3049402dbf7b59e1e628ca2ab
matthiasdaues <md@matthiasdaues.de>
2021-12-06 11:14:05 +0100

    December 6, 2021 11:14 AM

--- main.js
+++ main.js
@@ -221,23 +221,5 @@ var legendCtrl = new ol.control.Legend({
   collapsed: false
 });
 
-map.addControl(legendCtrl);
-legend.on('select', function(e) {
-  if (e.index >= 0) console.log((e.symbol ? 'You click on symbol: ':'You click on row: ')+e.item.get('title')+' ('+e.index+')');
-  else console.log('You click on the title: '+e.item.get('title'));
-});
-legend.addItem({ title: 'Car placemark', feature: f0 });
-function addSVG(svg, scale) {
-  legend.addItem({ 
-    title: 'SVG icon', 
-    typeGeom: 'Polygon',
-    style: new ol.style.Style({
-      image: new ol.style.Icon({
-        src: svg || 'https://upload.wikimedia.org/wikipedia/commons/6/67/OpenLayers_logo.svg',
-        scale: scale || .3
-      })
-    })
-  });
-}
 
 

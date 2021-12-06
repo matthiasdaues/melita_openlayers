a7eb39bdacd2245b3675c5aca8528868d761d8dd
matthiasdaues <md@matthiasdaues.de>
2021-12-06 11:34:08 +0100

    December 6, 2021 11:34 AM

--- main.js
+++ main.js
@@ -211,6 +211,44 @@ new Map({
 });
 
 
+  // Style function
+  function getFeatureStyle (feature) {
+    var st= [];
+    // Shadow style
+    st.push (new ol.style.Style ({
+      image: new ol.style.Shadow ({
+        radius: 15
+      })
+    }));
+    var st1= [];
+    // Font style
+    st.push ( new ol.style.Style ({
+      image: new ol.style.FontSymbol({
+        form: "marker", 
+        glyph: 'fa-car', 
+        radius: 15, 
+        offsetY: -15,
+        fontSize: .7,
+        color: '#fff',
+        fill: new ol.style.Fill ({
+          color: 'blue'
+        }),
+        stroke: new ol.style.Stroke ({
+          color: '#fff',
+          width: 2
+        })
+      }),
+      stroke: new ol.style.Stroke ({
+        width: 5,
+        color: '#f00'
+      }),
+      fill: new ol.style.Fill ({
+        color: [255, 0, 0, 0.6]
+      })
+    }));
+    return st;
+  }
+
 // Define a new legend
 var legend = new ol.legend.Legend({
   title: 'Legend',

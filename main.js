944cc1b78df7a5fead5deca7cec70f3e98fa8448
matthiasdaues <md@matthiasdaues.de>
2021-12-08 15:41:36 +0100

    December 8, 2021 3:41 PM

--- main.js
+++ main.js
@@ -249,26 +249,25 @@ map.addControl(scaleline);
 // add zoom to extent
 var zoom2extent = new ZoomToExtent({});
 map.addControl(zoom2extent);
-    // // add legend
-    // var legend = new ol_legend_Legend({
-    //   title: 'Coverage and Gateways',
-    //   className: 'ol-legend',
-    //   style: new Style({
-    //     fill: new Fill({color: 'rgba(1,1,1,1)'})
-    //   })
-    // })
-    // var legendCtrl = new ol_control_Legend({ 
-    //   legend: legend,
-    //   collapsed: false,
-    //   collapsible: true
-    // });
-    // map.addControl(legendCtrl);
-    // legend.addItem({ 
-    //   title: 'center point', 
-    //   feature: f0, 
-    //   typeGeom: Circle,
-    //     style: new Style({
-    //     fill: new Fill({color: 'rgba(1,1,1,1)'})
-    //   })
-    // });
-    // document.getElementByClass('ol-title').style.fontSize = '10pt';np
+var legend = new ol_legend_Legend({
+  title: 'Coverage and Gateways',
+  className: 'ol-legend',
+  style: new Style({
+  fill: new Fill({color: 'rgba(1,1,1,1)'})
+  })
+})
+var legendCtrl = new ol_control_Legend({ 
+  legend: legend,
+  collapsed: false,
+  collapsible: true
+});
+map.addControl(legendCtrl);
+legend.addItem({ 
+  title: 'center point', 
+  feature: f0, 
+  typeGeom: Circle,
+    style: new Style({
+    fill: new Fill({color: 'rgba(1,1,1,1)'})
+  })
+});
+document.getElementByClass('ol-title').style.color = 'red';

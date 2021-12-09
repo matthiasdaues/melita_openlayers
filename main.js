d26816d58ca15c05f1b7d4541b2b5c5cb60c5653
matthiasdaues <md@matthiasdaues.de>
2021-12-09 08:23:10 +0100

    December 9, 2021 8:23 AM

--- main.js
+++ main.js
@@ -249,25 +249,25 @@ map.addControl(scaleline);
 // add zoom to extent
 var zoom2extent = new ZoomToExtent({});
 map.addControl(zoom2extent);
-// var legend = new ol_legend_Legend({
-//   className: 'ol-test',
-//   title: 'Coverage and Gateways',
-//   style: new Style({
-//   fill: new Fill({color: 'rgba(1,1,1,1)'})
-//   })
-// })
-// var legendCtrl = new ol_control_Legend({ 
-//   className: 'ol-legend',
-//   legend: legend,
-//   collapsed: false,
-//   collapsible: true
-// });
-// map.addControl(legendCtrl);
-// legend.addItem({ 
-//   title: 'center point', 
-//   feature: f0, 
-//   typeGeom: Circle,
-//     style: new Style({
-//     fill: new Fill({color: 'rgba(1,1,1,1)'})
-//   })
-// });
+var legend = new ol_legend_Legend({
+  className: 'ol-test',
+  title: 'Coverage and Gateways',
+  style: new Style({
+  fill: new Fill({color: 'rgba(1,1,1,1)'})
+  })
+})
+var legendCtrl = new ol_control_Legend({ 
+  className: 'ol-legend',
+  legend: legend,
+  collapsed: false,
+  collapsible: true
+});
+map.addControl(legendCtrl);
+legend.addItem({ 
+  title: 'center point', 
+  feature: f0, 
+  typeGeom: Circle,
+    style: new Style({
+    fill: new Fill({color: 'rgba(1,1,1,1)'})
+  })
+});

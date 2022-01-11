bc65f400ffc3575d899b83cb0b99c04176d01381
matthiasdaues <md@matthiasdaues.de>
2022-01-11 08:55:51 +0100

    January 11, 2022 8:55 AM

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ const locationStyle = function (feature) {
     "1": locationStyleActive,
     "2": locationStyleActive,
   };
-  return styleTable[properties.getElementByID("status")]
+  return styleTable[properties["gateway"]["status"]]
 };
 // 01 2 2 coverage styles
 // 01 2 2 1 potential coverage over all available locations

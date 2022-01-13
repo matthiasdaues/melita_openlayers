314221184e3f7ad6fe1a6d1d255ee571f43f8277
matthiasdaues <md@matthiasdaues.de>
2022-01-13 10:48:02 +0100

    January 13, 2022 10:48 AM

--- main.js
+++ main.js
@@ -262,7 +262,36 @@ const locationsMVT = new VectorTileLayer({
   style: locationStyle, 
 });
 
+// /**
+//  * Elements that make up the popup.
+//  */
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
 
+
+//  /**
+//   * Add a click handler to hide the popup.
+//   * @return {boolean} Don't follow the href.
+//   */
+closer.onclick = function() {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+
+//  /**
+//   * Create an overlay to anchor the popup to the map.
+//   */
+var overlay = new ol.Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250
+  }
+});
+ 
 
 
 // put all on a map
